7d7e42fafec575f58fa5ee860c5d466c9db38b0d
WolfLee (TestofTime) <chrispekoske@hotmail.com>
2018-06-22 08:11:07 -0500

    Consider deleting these extra spaces.
     Line ends in whitespace. Consider deleting these extra spaces.  (lines of code = 1)

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -214,7 +214,7 @@ QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx)
         }
 
         strHTML += "<b>" + tr("Net amount") + ":</b> " + BitcoinUnits::formatWithUnit(BitcoinUnits::BTC, nNet, true) + "<br>";
-			
+
         //
         // Message
         //
@@ -226,7 +226,6 @@ QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx)
 
         strHTML += "<b>" + tr("Transaction ID") + ":</b> " + wtx.GetHash().ToString().c_str() + "<br>";
 
-		
 
         if (wtx.IsCoinBase() || wtx.IsCoinStake())
             strHTML += "<br>" + tr("Generated coins must mature 10 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.") + "<br>";
